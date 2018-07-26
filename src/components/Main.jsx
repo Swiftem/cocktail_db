02b0c9faa9ab50943385d787b4c5740295341a6c
Erinn Swift <swifterinn@gmail.com>
2018-07-26 19:27:31 -0400

    Removed Game Component to simplify

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Form from "./Form";
 import Profile from "./Profile";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import Game from "./Game";
 
 const Main = props => {
   return (
@@ -10,7 +9,7 @@ const Main = props => {
        <div>
          <Route exact path="/" component={ Form } />
          <Route path={"/Drink/:drink_id"} component={ Profile } />
-         <Route path="/Game" component={ Game }/>
+         
        </div>
      </BrowserRouter>
    )
